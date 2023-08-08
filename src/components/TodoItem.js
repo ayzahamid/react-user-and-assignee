1503b50462a3cc5f5ca7e12f17253361ffdc8c71
Ayza Hamid <ayzahamid222@gmail.com>
2023-08-08 19:32:36 +0500

    Update components and add assignee and tags (#3)
    * Replaced class components with functional components
    
    * Added table for completed tasks
    
    * Added tags and assignee
    
    * Updated readme file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,28 +1,29 @@
 import React from "react";
+import Tag from "./Tag";
 
-class TodoItem extends React.Component {
-  render() {
-    const completedStyle = {
-      fontStyle: "italic",
-      color: "#d35e0f",
-      opacity: 0.4,
-      textDecoration: "line-through"
-    };
+const TodoItem = (props) => {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#d35e0f",
+    opacity: 0.4,
+    textDecoration: "line-through"
+  };
 
-    const { completed, id, title } = this.props.todo;
+  const { completed, id, title, assignee, tags } = props.todo;
 
-    return (
-      <li className="todo-item">
-        <input
-          type="checkbox"
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
-        />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </li>
-    );
-  }
+  return (
+    <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
+      />
+      <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
+      <p>{assignee && `Assigned To: ${assignee}` }</p>
+      <p><Tag tags={tags}></Tag></p>
+    </li>
+  );
 }
 
 export default TodoItem;
